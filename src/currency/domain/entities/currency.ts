98f2d701d75394d3c17764c657b8e656a5173fd6
Kaius Rodrigues <kaiusdiego@gmail.com>
2023-03-30 00:01:37 -0300

    feat: create currency properties

--- src/currency/domain/entities/currency.ts
+++ src/currency/domain/entities/currency.ts
@@ -1,6 +1,27 @@
+export type CurrencyProperties = {
+  name: string; //eg: Real, USA Dollar
+  iso_code: string; //eg: BRL, USD, etc
+  quotation: number;
+  is_active?: boolean;
+  created_at?: Date;
+};
 
-export class Currency{
+export class Currency {
+  constructor(readonly props: CurrencyProperties) {}
 
-  constructor(public name: string){}
+  get name(){
+   return this.props.name 
+  }
 
+  get iso_code(){
+    return this.props.iso_code
+  }
+
+  get is_active(){
+    return this.props.is_active
+  }
+
+  get created_at(){
+    return this.props.created_at
+  } 
 }
