cfa9c1b9e3b8e1aad39f453b7196c56cdf9c53c6
Kaius Diego <kaiusdiego@gmail.com>
2023-04-07 13:37:03 -0300

    ref: creating currency repository namespace

--- src/@core/src/currency/domain/repository/currency.repository.ts
+++ src/@core/src/currency/domain/repository/currency.repository.ts
@@ -1,4 +1,9 @@
 import { RepositoryInterface } from "../../../@seedwork/domain/repository/repository-contracts";
 import { Currency } from "../entities/currency";
 
-export default interface CurrencyRepository extends RepositoryInterface<Currency>{}
+export namespace CurrencyRepository {
+  export interface Repository extends RepositoryInterface<Currency>{}
+
+}
+
+export default CurrencyRepository
