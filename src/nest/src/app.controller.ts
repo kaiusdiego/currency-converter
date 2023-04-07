7cfde562397dc70ce0aae6aab942128e13d63f52
Kaius Diego <kaiusdiego@gmail.com>
2023-04-07 14:15:22 -0300

    ref: activating basic tests in nest

--- src/nest/src/app.controller.ts
+++ src/nest/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+import { Currency } from 'sbf-converter/currency/domain'
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    console.log(Currency);
+    
     return this.appService.getHello();
   }
 }
--- src/nest/src/app.module.ts
+++ src/nest/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CurrenciesModule } from './currencies/currencies.module';
 
 @Module({
-  imports: [],
+  imports: [CurrenciesModule],
   controllers: [AppController],
   providers: [AppService],
 })
