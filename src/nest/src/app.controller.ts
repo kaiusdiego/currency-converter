a664cf08ec5e614d6500eb831baa13bcb36bfbb5
Kaius Diego <kaiusdiego@gmail.com>
2023-04-10 15:32:38 -0300

    feat: adding currency controller unit tests

--- src/nest/src/app.controller.ts
+++ src/nest/src/app.controller.ts
@@ -1,16 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-import { Currency } from 'sbf-converter/currency/domain'
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    console.log(Currency);
-    
+  getHello(): string {    
     return this.appService.getHello();
   }
 }
--- src/nest/src/currencies/currencies.controller.spec.ts
+++ src/nest/src/currencies/currencies.controller.spec.ts
@@ -1,20 +1,123 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateCurrencyUseCase, ListCurrencyUseCase, UpdateCurrencyUseCase } from 'sbf-converter/currency/application';
 import { CurrenciesController } from './currencies.controller';
 import { CurrenciesService } from './currencies.service';
+import { CreateCurrencyDto } from './dto/create-currency.dto';
+import { UpdateCurrencyDto } from './dto/update-currency.dto';
 
 describe('CurrenciesController', () => {
-  let controller: CurrenciesController;
+  let controller: CurrenciesController
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CurrenciesController],
-      providers: [CurrenciesService],
-    }).compile();
-
-    controller = module.get<CurrenciesController>(CurrenciesController);
+    controller = new CurrenciesController()
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should creates a new currency', async () => {
+    const expectedOutput: CreateCurrencyUseCase.Output = {
+      id: '62236c07-8783-4f1e-b33a-a7c37db9b02c',
+      iso_code_from: "USD",
+      iso_code_to: "BRL",
+      quotation: 5,
+      is_active: true,
+      created_at: new Date()
+    }
+
+    const MockCreateUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput))
+    }
+
+    //@ts-expect-error
+    controller["createUseCase"] = MockCreateUseCase;
+
+    let input: CreateCurrencyDto = {
+      iso_code_from: 'USD',
+      iso_code_to: 'BRL',
+      quotation: 5,
+    }
+
+    const output = await controller.create(input)
+
+    expect(MockCreateUseCase.execute).toHaveBeenCalledWith(input)
+
+    expect(expectedOutput).toStrictEqual(output);
   });
+
+  it('should update a currency', async () => {
+
+    const id = '62236c07-8783-4f1e-b33a-a7c37db9b02c'
+
+    const expectedOutput: UpdateCurrencyUseCase.Output = {
+      id,
+      iso_code_from: "USD",
+      iso_code_to: "BRL",
+      quotation: 5,
+      is_active: true,
+      created_at: new Date()
+    }
+
+    const MockUpdateUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput))
+    }
+
+    //@ts-expect-error
+    controller["updateUseCase"] = MockUpdateUseCase;
+
+    let input: UpdateCurrencyDto = {
+      iso_code_from: 'USD',
+      iso_code_to: 'BRL',
+      quotation: 5.5,
+    }
+
+    const output = await controller.update(id,input)
+
+    expect(MockUpdateUseCase.execute).toHaveBeenCalledWith({id, ...input})
+
+    expect(expectedOutput).toStrictEqual(output)
+  });
+
+  it('should get a conversion', async () => {
+     
+    const expectedOutput: ListCurrencyUseCase.Output = [
+      {iso_code: 'USD', price: 20}
+    ]
+
+    const MockConversionUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput))
+    }
+
+    //@ts-expect-error
+    controller["listUseCase"] = MockConversionUseCase;
+    const iso_code = 'BRL'
+    const price = 100
+
+    const output = await controller.findOne(iso_code,price)
+    console.log('output',output);
+    
+    expect(MockConversionUseCase.execute)
+    .toHaveBeenCalledWith({iso_code,price})
+
+    expect(expectedOutput).toStrictEqual(output)
+
+  });
+
+  it('should delete a currency', async () => {
+    const expectedOutput = undefined
+
+    const mockDeleteUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput))
+    }
+
+    //@ts-expect-error
+    controller['deleteUseCase'] = mockDeleteUseCase
+
+    const id = '62236c07-8783-4f1e-b33a-a7c37db9b02c'
+
+    expect(controller.remove(id)).toBeInstanceOf(Promise)
+
+    const output = await controller.remove(id)
+    expect(mockDeleteUseCase.execute).toBeCalledWith({id})
+    expect(expectedOutput).toStrictEqual(output)
+  });
+
+  
 });
